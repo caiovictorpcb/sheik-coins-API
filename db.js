185f37c8286a9be52176a9b8bcbe68a952ec00a0
Caio Victor <caiovictor.pcb@gmail.com>
2025-10-31 12:15:06 -0300

    fix

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv'
+import pg from 'pg';
 
 dotenv.config()
 
 const sequelize = new Sequelize(process.env.DATABASE_URI, {
     dialect: 'postgres',
+    dialectModule: pg,
     protocol: 'postgres',
     dialectOptions: {
         ssl: {
